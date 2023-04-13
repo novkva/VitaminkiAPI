fa07e1b2ae2f184a70dea9815f9f09e97216ca11
novkva <brainforfreedom@gmail.com>
2023-04-13 22:41:42 +0300

    new migrations

---
+++ DAL/Migrations/20230412131247_AddColumnToIngProd.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnToIngProd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(name: "MassInProduct", table: "IngredientProduct", nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
